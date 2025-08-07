29dbc17627af8b9f665002eecaad636101f649e0
kaustubh <kaustubhsoni34@gmail.com>
2025-08-07 18:20:01 +0530

    conditional rendering on navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="bg-transparent flex justify-evenly text-2xl items-center gap-10 relative top-0 z-99 text-[#C70039] mt-10 font-bold">
         <Link to={"/about"}>About</Link>
         <Link to={"/services"}>Our Services</Link>
-        <Link to={"/stories"}>Client Stories</Link>
+        <Link to={"/stories"}>Client Stories</Link>  
         <Link to={"/"}>
           <img src={weblogo} alt="" className="h-[8em]" />
         </Link>
